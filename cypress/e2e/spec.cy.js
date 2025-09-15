65eb26aa23956ca719ec90a06fbc7a6be3651f3f
Emerson <70976306+emerdesenv@users.noreply.github.com>
2025-09-15 16:08:13 -0300

    hotfix: Ajuste de testes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,5 @@
-describe('template spec', () => {
-    it('passes', () => {
-        cy.visit('https://www.cypress.io/')
+describe('Template de Teste com Cypress', () => {
+    it('Acesso IP Oracle', () => {
+        cy.visit('http://168.138.129.255:8080/')
     });
 });
