7ec98fe0bf9205585915ffc548ce6bef832d6e4b
Emerson <70976306+emerdesenv@users.noreply.github.com>
2026-01-08 16:55:44 -0300

    refactor: Ajustes de teste

--- src/inicial.php
+++ src/inicial.php
@@ -1,9 +1,9 @@
 <?php
-    $sql = "SELECT * FROM texto WHERE (id_praca = ".$id_praca." AND Ativo = 'S') OR Comunicado = 1 ORDER BY NrOrdem";
+    $sql = "SELECT * FROM usuario WHERE idUsuario = $id_usuario";
     
     if($result = $mysqli->query($sql)) {
         while($row = $result->fetch_assoc()) {
-            //Faz nada, ainda nada
+            //Faz nada, ainda
         }
     }
 ?>
